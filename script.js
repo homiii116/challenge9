8a1dd6bb884c089ddd8c4e8ec6400e338b159001
miho yamagata <miho.yamagata@gmail.com>
2020-02-24 20:24:02 +0900

    edit

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 
 //画像情報のオブジェクトを格納した配列
 const picArray = [
-{src: '', title: ''},
-{src: '', title: ''},
-{src: '', title: ''},
-{src: '', title: ''},
-{src: '', title: ''},
-{src: '', title: ''}
+{src: './images/pic_1.jpg', title: 'picture 1'},
+{src: './images/pic_2.jpg', title: 'picture 2'},
+{src: './images/pic_3.jpg', title: 'picture 3'},
+{src: './images/pic_4.jpg', title: 'picture 4'},
+{src: './images/pic_5.jpg', title: 'picture 5'},
+{src: './images/pic_6.jpg', title: 'picture 6'}
 ];
 
 
@@ -16,7 +16,16 @@ let playingID = 0;
 
 //写真を切り替える関数
 //関数名「changePicture」
-
+function changePicture() {
+  if (playingID > picArray.length) {
+    playingID = 0;
+  } else {
+    playingID ++;
+    document.getElementById('pics').src = picArray[0].src
+    document.getElementById('pic-title').innterHTML = picArray[0].title
+    } 
+}
 
 // ボタンを押してスライドショーを再生・停止する関数
 //関数名「playSlidedeshow」
+
